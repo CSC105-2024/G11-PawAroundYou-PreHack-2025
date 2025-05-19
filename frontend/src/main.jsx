749ba8d2b9aea60895713ab14944a16173d22e53
pluu <tonplu110@gmail.com>
2025-05-19 11:35:24 +0700

    Home,SignUp,Login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,18 +2,27 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
 
 import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/", // Home route
-        element: <App />, // Render the App component
+        element: <HomePage />, // Render the App component
     },
     {
         path: "/home", // Home route
         element: <HomePage />, // Render the App component
+    },
+    {
+        path: "/signup",
+        element: <SignUpPage />,
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
     },
 ]);
 
